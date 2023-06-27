267576a6262cc9143c035a5a7f380ac6fcb98690
aaaa1ass <aaaa1ass@gmail.com>
2023-06-27 21:46:54 +0900

    2023/06/27
    스마트 포인터

---
+++ Solution/Solution/(2023.6.26)업캐스팅.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include "Class Header/Marine.h"
+#include "Class Header/Ghost.h"
+
+using namespace std;
+
+void Recovery(Unit* unit)
+{
+	unit->SetHP(200);
+}
+
+int main()
+{
+	// 업 캐스팅
+	Marine marine;
+	Ghost ghost;
+
+	Unit* uptr = &marine;
+	uptr->Skill();
+
+	uptr = &ghost;
+	uptr->Skill();
+
+	Recovery(&marine);
+	cout << "Marine HP: " << marine.GetHP() << endl;
+	Recovery(&ghost);
+	cout << "Ghost HP: " << ghost.GetHP() << endl;
+
+	return 0;
+}
+
---
+++ Solution/Solution/Class Header/Item.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <memory>
+
+class Item
+{
+public:
+	std::weak_ptr<Item> ptr;
+
+	Item();
+	~Item();
+};
+
