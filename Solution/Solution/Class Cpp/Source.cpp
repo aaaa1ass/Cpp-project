f6469a3d85bc9ca6211c04a5c096afdd4d90a1c3
aaaa1ass <aaaa1ass@gmail.com>
2023-07-05 21:46:47 +0900

    2023/07/05
    컨테이너 어댑터

--- Solution/Solution/Class Cpp/Source.cpp
+++ Solution/Solution/Class Cpp/Source.cpp
@@ -1,34 +1,76 @@
 ﻿#include <iostream>
 
-//STL 라이브러리
-#include<string>
+#include <queue>
+#include <stack>
+#include <vector>
+
 
 using namespace std;
 
-
-
-
 int main()
 {
-#pragma region 문자열 클래스 std::string
-	std::string string("문자열");
-	std::string name;
+#pragma region 컨테이너 어댑터
+	//기존 컨테이너의 인터페이스를 제한하여 만든, 기능 제한되거나 변형된 컨테이너
+	// std::queue<int> queue;
+	// 
+	// queue.push(10);
+	// queue.push(20);
+	// queue.push(30);
+	// queue.push(40);
+	// queue.push(50);
+	// 
+	// 
+	// while (queue.empty() == 0)
+	// {
+	// 	cout << queue.front() << " ";
+	// 	queue.pop();
+	// }
 
-	//std::cout << "string 변수의 값: " << string << endl;
-	//
-	//string = "클래스";
-	//
-	//std::cout << "string 변수의 값: " << string << endl;
+	//queue.pop(); 컨테이너에 데이터가 없을 때 pop()을 시도하면 런타임 에러 발생
+	
 
-	std::cout << "string 변수의 값을 입력: ";
-	std::cin >> string;
+	//std::swap
+	// int a = 10;
+	// int b = 20;
+	// 
+	// std::swap(a, b);
+	// 
+	// cout << "a: " << a << endl;
+	// cout << "b: " << b << endl;
 
-	std::cout << "name 변수의 값을 입력: ";
-	std::cin >> name;
+	//std::stack
+	// std::stack<int> stack;
+	// stack.push(10);
+	// stack.push(20);
+	// stack.push(30);
+	// stack.push(40);
+	// stack.push(50);
+	// 
+	// while (stack.size() != 0)
+	// {
+	// 	cout << stack.top() << endl;
+	// 	stack.pop();
+	// }
 
-	std::cout << "string 변수의 값: " << string << endl << "name 변수의 값: " << name <<std::endl;
-	//띄어쓰기 고치기
+	// std::iterator 반복자
+	// std::vector<int> vector;
+	// 
+	// vector.push_back(10);
+	// vector.push_back(20);
+	// vector.push_back(30);
+	// 
+	// std::vector<int>::iterator iter;
+	// vector.insert(vector.begin() + 1, 999);
+	// vector.erase(vector.begin() + 3);
+	// 
+	// // iter가 가리키는 값
+	// for (iter = vector.begin(); iter != vector.end(); iter++)
+	// {
+	// 	cout << *iter << endl;
+	// }
+
 #pragma endregion
+
 
 	return 0;
 }
